b7fbf7e865d9fdb98570d5d5345505fb8486ea06
russ <russmorris1982@gmail.com>
2019-12-16 21:23:41 +0000

    Added app module

---
+++ buildSrc/src/main/java/Api.kt
@@ -0,0 +1,6 @@
+object API {
+    val weatherServiceKey = "" //TODO: DO NOT COMMIT TO GITHUB!!
+    val weatherServiceEndpoint = "https://worksample-api.herokuapp.com/"
+
+    val playServicesPlacesKey = "" //TODO: DO NOT COMMIT TO GITHUB!!
+}
